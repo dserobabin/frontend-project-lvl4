287186b85bb3ffc214e97bfcf51fd7a6d7ef2768
Danil <dan.serobabin@gmail.com>
2023-08-26 13:59:14 +0300

    feat: add error page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Chat from './Chat.jsx';
 import Login from './Login.jsx';
+import Error from './Error.jsx';
 import routes from '../routes.js';
 
 const App = () => (
@@ -8,6 +9,7 @@ const App = () => (
     <Routes>
       <Route path={routes.ChatPath()} element={<Chat />} />
       <Route path={routes.LoginPath()} element={<Login />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ frontend/src/components/Error.jsx
@@ -0,0 +1,8 @@
+const Error = () => (
+  <>
+    <h1>Oops!</h1>
+    <p>Sorry, an unexpected error has occurred.</p>
+  </>
+);
+
+export default Error;
