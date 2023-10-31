d45900b2e0a9862fbae8b5ae2d70a60f9ec93123
Danil <dan.serobabin@gmail.com>
2023-10-31 09:08:11 +0300

    refactor: changed the styles on the login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,7 +4,7 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
-import login from '../assets/login.jpeg';
+import loginImage from '../assets/login.jpeg';
 import routes from '../routes';
 import useAuth from '../hooks/index.js';
 
@@ -45,41 +45,37 @@ const Login = () => {
     <div className="container-fluid h-100">
       <div className="row justify-content-center align-items-center h-100">
         <div className="col-12 col-md-8 col-xxl-6">
-          <div className="card">
+          <div className="card shadow-sm">
             <div className="card-body row p-5">
-              <div className="col-12 col-md-6">
-                <div className="d-flex justify-content-center align-items-center">
-                  <img className="rounded-circle" alt="tota" src={login} />
-                </div>
+              <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
+                <img className="rounded-circle" alt="Войти" src={loginImage} />
               </div>
-              <div className="col-12 col-md-6">
+              <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
                 <h1 className="text-center mb-4">Войти</h1>
-                <Form onSubmit={formik.handleSubmit}>
-                  <Form.Group className="form-floating mb-3">
-                    <Form.Control
-                      id="username"
-                      name="username"
-                      onChange={formik.handleChange}
-                      placeholder="Ваш ник"
-                      isInvalid={authFailed}
-                      ref={inputRef}
-                    />
-                    <Form.Label htmlFor="username">Ваш ник</Form.Label>
-                  </Form.Group>
-                  <Form.Group className="form-floating mb-3">
-                    <Form.Control
-                      id="password"
-                      name="password"
-                      onChange={formik.handleChange}
-                      placeholder="Пароль"
-                      isInvalid={authFailed}
-                    />
-                    <Form.Label htmlFor="password">Пароль</Form.Label>
-                    <Form.Control.Feedback type="invalid">Неверное имя пользователя или пароль</Form.Control.Feedback>
-                  </Form.Group>
-                  <Button type="submit" variant="outline-primary" className="w-100 mb-3">Войти</Button>
-                </Form>
-              </div>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    id="username"
+                    name="username"
+                    onChange={formik.handleChange}
+                    placeholder="Ваш ник"
+                    isInvalid={authFailed}
+                    ref={inputRef}
+                  />
+                  <Form.Label htmlFor="username">Ваш ник</Form.Label>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    id="password"
+                    name="password"
+                    onChange={formik.handleChange}
+                    placeholder="Пароль"
+                    isInvalid={authFailed}
+                  />
+                  <Form.Label htmlFor="password">Пароль</Form.Label>
+                  <Form.Control.Feedback type="invalid">Неверное имя пользователя или пароль</Form.Control.Feedback>
+                </Form.Group>
+                <Button type="submit" variant="outline-primary" className="w-100 mb-3">Войти</Button>
+              </Form>
             </div>
             <div className="card-footer p-4">
               <div className="text-center">
