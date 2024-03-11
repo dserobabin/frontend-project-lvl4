c0e1fdb46ebd3b39edce0acd2cc406806e20a91c
dserobabin <dan.serobabin@gmail.com>
2024-03-11 18:22:01 +0300

    feat: add paths "/" and "/login"

--- frontend/src/componets/App.jsx
+++ frontend/src/componets/App.jsx
@@ -1,3 +1,14 @@
-const App = () => <div>Chat</div>;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import MainPage from './MainPage';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ frontend/src/componets/MainPage.jsx
@@ -0,0 +1,3 @@
+const MainPage = () => <div>Chat</div>;
+
+export default MainPage;
